cdb93b387b11f703d170f34029cdd51778fa4f98
Alborz Borzoo <alborzborzoo13@gmail.com>
2024-08-12 22:22:20 +0330

    test python file: first ideas

--- files/test.py
+++ files/test.py
@@ -6,3 +6,25 @@ def calc(txt:str):
 print("please enter what you want to be calculated:")
 text = input("-> ")
 
+OPERATORS = {"+":"addition",
+              "-":"subtraction",
+              "/":"devision",
+              "*":"multiplication"}
+
+operations = []
+
+numbers = []
+
+current_number = ""
+
+for char in text:
+    if char in OPERATORS:
+        operations.append(char)
+        numbers.append(current_number)
+        current_number = ""
+    else:
+        current_number += char
+
+numbers.append(current_number)
+
+print(numbers, operations)
